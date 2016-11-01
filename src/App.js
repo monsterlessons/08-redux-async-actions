9473ed86e5f145bf36a2729e55f41ca0d42a532a
Alexander Kochergin <monsterlessons@gmail.com>
2016-11-01 22:15:51 +0100

    05-react-form

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
 
-import Header from './Header';
-
-const menu = [
-  {
-    link: '/articles',
-    label: 'Articles'
-  },
-  {
-    link: '/contacts',
-    label: 'Contacts'
-  },
-  {
-    link: '/posts',
-    label: 'Posts'
-  }
-];
+import RegistationForm from './RegistrationForm';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header items={menu} />
+        <RegistationForm />
       </div>
     );
   }
