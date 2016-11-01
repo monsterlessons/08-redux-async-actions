3cbb5dc0c8cf2cb445ed0b222db3c1215d9f63e5
Alexander Kochergin <monsterlessons@gmail.com>
2016-11-01 22:16:33 +0100

    05-react-form added form

---
+++ src/RegistrationForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class RegistationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('form is submitted. Email value is', this.state.email);
+  }
+
+  handleEmailChange(event) {
+    console.log('email was changed', event.target.value);
+    this.setState({email: event.target.value});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <button>Save</button>
+      </form>
+    );
+  }
+}
+
+export default RegistationForm;
