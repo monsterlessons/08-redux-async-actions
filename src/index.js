0a3f171184ca2169850a1e12f515172a00b654c5
Alexander Kochergin <monsterlessons@gmail.com>
2016-11-08 21:57:44 +0100

    01 redux intro

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
+
+import { createStore } from 'redux';
+
+function playlist(state = [], action) {
+  if (action.type === 'ADD_TRACK') {
+    return [
+      ...state,
+      action.payload
+    ];
+  }
+  return state;
+}
+
+const store = createStore(playlist);
+
+store.subscribe(() => {
+  console.log('subscribe', store.getState());
+})
+
+store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
