ecd2ef56591cb34545ea82412a5a802999b91dc6
Alexander Kochergin <monsterlessons@gmail.com>
2016-11-26 14:20:37 +0100

    05 redux-devtools

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ function playlist(state = initialState, action) {
   return state;
 }
 
-const store = createStore(playlist);
+const store = createStore(playlist, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
